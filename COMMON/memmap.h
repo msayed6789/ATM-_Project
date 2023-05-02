a6819bfbd8edfd1a3dc1bac6f4ee216a2c15302a
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-05-02 04:36:58 +0300

    Update memmap.h

--- COMMON/memmap.h
+++ COMMON/memmap.h
@@ -82,9 +82,80 @@
 #define OCF2  7 //Output Compare Flag bit
 #define TOV2  6 //Timer2 Overflow Flag bit
 /******************************************************************************/
+/* USART Baud Rate Register Low */
+#define UBRRL    (*(volatile unsigned char*)0x29)
+
+/* USART Control and Status Register B */
+#define UCSRB   (*(volatile unsigned char*)0x2A)
+
+#define    RXCIE        7
+#define    TXCIE        6
+#define    UDRIE        5
+#define    RXEN         4
+#define    TXEN         3
+#define    UCSZ         2
+#define    UCSZ2        2
+#define    RXB8         1
+#define    TXB8         0
+/* USART Control and Status Register A */
+#define UCSRA    (*(volatile unsigned char*)0x2B)
+
+#define    RXC          7
+#define    TXC          6
+#define    UDRE         5
+#define    PE           2
+#define    FE           4
+#define    DOR          3
+#define    UPE          2
+#define    U2X          1
+#define    MPCM         0
+/* USART I/O Data Register */
+#define UDR       (*(volatile unsigned char*)0x2C)
+
+/* USART Baud Rate Register High */
+#define UBRRH    (*(volatile unsigned char*)0x40)
+
+/* USART Control and Status Register C */
+#define UCSRC     (*(volatile unsigned char*)0x40)
+
+/* USART Register C */
+#define    URSEL        7
+#define    UMSEL        6
+#define    UPM1         5
+#define    UPM0         4
+#define    USBS         3
+#define    UCSZ1        2
+#define    UCSZ0        1
+#define    UCPOL        0
+
+/*********************************************************************************/
+/* Interrupt vectors */
+/* External Interrupt Request 0 */
+#define INT0_vect			__vector_1
+/* External Interrupt Request 1 */
+#define INT1_vect			__vector_2
+/* External Interrupt Request 2 */
+#define INT2_vect			__vector_3
+/* Timer/Counter1 Capture Event */
+#define TIMER1_ICU_vect		__vector_6
+/* Timer/Counter1 Compare Match A */
+#define TIMER1_OCA_vect		__vector_7
+/* Timer/Counter1 Compare Match B */
+#define TIMER1_OCB_vect		__vector_8
+/* Timer/Counter1 Overflow */
+#define TIMER1_OVF_vect			__vector_9
+/* USART, Rx Complete */
+#define UART_RX_vect			__vector_13
+/* USART Data Register Empty */
+#define UART_UDRE_vect			__vector_14
+/* USART, Tx Complete */
+#define UART_TX_vect			__vector_15
 /******************************************************************************/
 /*interrupt functions*/
 
+#  define ISR(vector,...)            \
+void vector (void) __attribute__ ((signal))__VA_ARGS__ ; \
+void vector (void)
 # define sei()  __asm__ __volatile__ ("sei" ::)
 # define cli()  __asm__ __volatile__ ("cli" ::)
 
