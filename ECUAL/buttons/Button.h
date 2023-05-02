a843552497dcf86e379942d6a7651c3aa2a10ad6
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-05-02 04:27:34 +0300

    Add files via upload

---
+++ ECUAL/buttons/Button.h
@@ -0,0 +1,37 @@
+/*
+ * Button.h
+ *
+ * Created: 4/5/2023 4:19:28 AM
+ *  Author: Mohamed Sayed
+ */ 
+
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+#include "DIO_Interface.h"
+#include "timer.h"
+
+//#include "dio.h"
+
+#define  Time_Test  2      //Time in sec
+/*******************************************************************************/
+typedef enum{
+	Pressed,
+	NotPressed	
+}Button_Status;
+
+typedef enum{
+	Pressed_UnderTime=2,
+	Pressed_OverTime=4
+}Button_Status_Time;
+
+void button_init(PIn_name pin);
+
+Button_Status Button_Check(u8 Button);
+
+Button_Status_Time Button_Check_Time(u8 Button);
+
+
+
+#endif /* BUTTON_H_ */
