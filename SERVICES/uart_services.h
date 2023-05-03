f6d3d8d35f4b87f0bc9ac30f2a387fca1eb880f0
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-05-03 21:02:41 +0300

    Update uart_services.h

--- SERVICES/uart_services.h
+++ SERVICES/uart_services.h
@@ -17,15 +17,15 @@
 /*******************************************************Transmitter************************************************************************************************/
 void uart_transmitString(u8*str);
 void uart_transmitString_Interrupt(u8*str);
-void uart_transmitNumber(u32 num);
-void uart_transmitNumber_Interrupt(u32 num);
+void uart_transmitNumber_MCU(u32 num);
+void uart_transmitNumber_MCU_Interrupt(u32* num);
 
 
 /*******************************************************Receiver*********************************************************************************************/
 void uart_recieverString(u8*str);
 void uart_recieverString_Interrupt(u8*str);
-void uart_recieveNumber(u16* num);
-void uart_recieveNumber_Interrupt(u16* num);
+void uart_recieveNumber_MCU(u32* num);
+void uart_recieveNumber_MCU_Interrupt(u32* num);
 
 
 #endif /* UART_SERVICES_H_ */
